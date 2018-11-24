ce03cc4c54352d1e5e2a30619c51772b06e0b5d5
João Gabriel Lima Moraes <joaofeirense@gmail.com>
2018-11-24 18:14:01 -0300

    Corpo da Classe jogador
    Atributos, gets e Sets

--- src/Model/Jogador.java
+++ src/Model/Jogador.java
@@ -1,5 +1,32 @@
 package Model;
 
 public class Jogador {
+	private Carta Mao[];
+	private String Cor;
+	private int Exercitos_Disponiveis;
+
+	public Carta[] getMao() {
+		return Mao;
+	}
+
+	public void setMao(Carta mao[]) {
+		Mao = mao;
+	}
+
+	public String getCor() {
+		return Cor;
+	}
+
+	public void setCor(String cor) {
+		Cor = cor;
+	}
+
+	public int getExercitos_Disponiveis() {
+		return Exercitos_Disponiveis;
+	}
+
+	public void setExercitos_Disponiveis(int exercitos_Disponiveis) {
+		Exercitos_Disponiveis = exercitos_Disponiveis;
+	}
 
 }
