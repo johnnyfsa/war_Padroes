2a0900229966d3486b956351ddc05f46b819c5a6
Pedro Mota <pedromotafsa@hotmail.com>
2018-12-02 20:44:21 -0300

    Combate Resultados

--- src/controller/CombatController.java
+++ src/controller/CombatController.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 import java.util.Scanner;
 
 import model.Estado;
@@ -17,14 +18,22 @@ public class CombatController {
 		teste.add(2);
 		teste.add(6);
 		for (int i = 0; i < teste.size(); i++) {
-//			System.out.println(teste.get(i));
+			System.out.println(teste.get(i));
 		}
 		
 		Collections.sort(teste,Collections.reverseOrder());
 		
 		for (int i = 0; i < teste.size(); i++) {
-//			System.out.println(teste.get(i));
+			System.out.println(teste.get(i));
 		}	
+	}
+		
+	public int setTropas(int quantidade) {
+		if (quantidade >=3) {
+			return 3;
+		} else {
+			return quantidade;
+		}
 	}
 	
 	public void combate(Jogador jogador, Tabuleiro mapa) throws Exception {
@@ -51,7 +60,41 @@ public class CombatController {
 			}			
 		
 //			Entra aqui lógica para ataque!
-		
+			
+			int tropasAtacantes = setTropas(origem.getQuantidade_de_Tropas()-1);
+			int tropasDefensoras = setTropas(destino.getQuantidade_de_Tropas());
+			
+			ArrayList<Integer> dadosAtaque = new ArrayList<Integer>();					
+			ArrayList<Integer> dadosDefesa = new ArrayList<Integer>();
+			
+			
+			for (int i = 0; i < dadosAtaque.size();i++) {
+				Random rn = new Random();
+				dadosAtaque.add(rn.nextInt(6)+1);			
+			}
+			
+			for (int i = 0; i < dadosDefesa.size();i++) {
+				Random rn = new Random();
+				dadosDefesa.add(rn.nextInt(6)+1);						
+			}
+			
+			Collections.sort(dadosAtaque,Collections.reverseOrder());
+			Collections.sort(dadosDefesa,Collections.reverseOrder());
+			
+			int combates = Math.min(dadosAtaque.size(), dadosDefesa.size());
+			
+			
+			for (int i = 0; i < combates; i++) {
+				System.out.println("Luta " + (i+1) + ": Ataque: " + dadosAtaque.get(i) + " vs " + dadosDefesa.get(i) + " Defesa");
+				if (dadosAtaque.get(i) <= dadosDefesa.get(i)) {
+					System.out.println("Defesa venceu a luta " + (i+1));					
+				} else {
+					System.out.println("Ataque venceu a luta " + (i+1));					
+				}
+				System.out.println("------------------------------");
+			}
+			
+			
 		} while (check);
 		
 		
